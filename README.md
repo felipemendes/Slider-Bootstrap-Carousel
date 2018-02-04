86625dc74ac1b63a0d64e10dc55af3519306a95f
felipemendes <f.ribeiromendes@gmail.com>
2018-02-04 00:43:42 -0200

    Plugin init

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# bootstrap-carousel-for-wordpress
+# Bootstrap Carousel for WordPress
 Bootstrap Carousel 4 for WordPress with image link and categories
+
+# Installation
+
+1. Upload the plugin folder 'bootstrap-carousel-for-wordpress' into the '/wp-content/plugins/' directory.
+2. Activate the plugin through the 'Plugins' screen in WordPress.
---
+++ bootstrap-carousel-for-wordpress.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://github.com/felipemendes
+ * @since             1.0.0
+ * @package           bootstrap-carousel-for-wordpress
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Bootstrap Carousel for WordPress
+ * Description:       Bootstrap 4 Carousel for WordPress 
+ * Version: 		  1.0.0
+ * Author:            Felipe Mendes
+ * Author URI:        https://github.com/felipemendes
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       bootstrap-carousel-for-wordpress
+ * Domain Path:       /languages
+ */
+
+add_action('init', 'bootstrap_carouse_for_wordpress_init');
+function bootstrap_carouse_for_wordpress_init()
+{
+	$singular 	= 'Carousel';
+	$plural 	= 'Carousels';
+
+	$labels = array(
+			'name'               	=> _x( $singular, 'post type general name', '' ),
+			'singular_name'      	=> _x( $singular, 'post type singular name', '' ),
+			'add_new'            	=> _x( 'Add', 'print', '' ),
+			'add_new_item'       	=> __( 'Add ' . $singular, '' ),
+			'edit_item'          	=> __( 'Edit ' . $singular, '' ),
+			'new_item'           	=> __( 'Add ' . $singular, '' ),
+			'all_items'          	=> __( 'All ' . $plural, '' ),
+			'view_item'          	=> __( 'View ' . $singular, '' ),
+			'search_items'       	=> __( 'Search ' . $plural, '' ),
+			'not_found'          	=> __( 'No ' . $singular . ' found', '' ),
+			'not_found_in_trash' 	=> __( 'No ' . $singular . ' found in Trash', '' ), 
+			'parent_item_colon'  	=> '',
+			'menu_name'          	=> __( $plural, '' )
+	);
+	
+	$args = array(
+			'labels'              	=> $labels,
+			'public'              	=> false,
+			'publicly_queryable'  	=> true,
+			'show_ui'             	=> true, 
+			'show_in_menu'        	=> true, 
+			'query_var'           	=> true,
+			'capability_type'     	=> 'post',
+			'has_archive'         	=> true, 
+			'hierarchical'       	=> false,
+			'menu_position'       	=> null,
+			'exclude_from_search' 	=> true,
+			'publicly_queryable'  	=> true,
+			'menu_icon' 			=> 'dashicons-format-gallery',
+			'supports'           	=> array( 'title', 'thumbnail' )
+	); 
+	register_post_type('bootstap_carousel', $args);
+}
